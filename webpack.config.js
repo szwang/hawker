c21fbf5bfb2e3e6b63b8cab99f11bb2182d8bb55
Suzanne Wang <suzanneswang@gmail.com>
2016-01-03 10:38:42 +0800

    [chore] add css build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -14,21 +15,6 @@ module.exports = {
 
   devtool: 'source-map',
 
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel?presets[]=react,presets[]=es2015']
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        loaders: ['style', 'css', 'sass']
-      }
-    ]
-  },
-
   plugins: [
     // new HtmlWebpackPlugin({
     //   template: 'app/index.tpl.html',
@@ -37,6 +23,24 @@ module.exports = {
     // }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('app.css', {
+      allChunks: true
+    })
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel?presets[]=react,presets[]=es2015']
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+      }
+    ]
+  }
 };
