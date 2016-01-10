edbe0f16e01736beadb8da3b53dc88814eadb9be
Suzanne Wang <suzanneswang@gmail.com>
2016-01-09 20:58:29 -0500

    [refactor] validation functions

--- src/js/components/AuthModals.react.js
+++ src/js/components/AuthModals.react.js
@@ -60,6 +60,71 @@ export class SignupModal extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.passwordValidationState = this.passwordValidationState.bind(this);
+    this.usernameValidationState = this.usernameValidationState.bind(this);
+    this.emailValidationState = this.emailValidationState.bind(this);
+    this.passwordConfirmValidationState = this.passwordConfirmValidationState.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  usernameValidationState() {
+    let usernameStyle;
+    let length = this.state.username.length;
+    if(length >= 4) {
+      usernameStyle = 'success';
+    } else if(length > 0) {
+      usernameStyle = 'error';
+    }
+    let usernameValid = usernameStyle === 'success';
+
+    return { usernameStyle, usernameValid };
+  }
+
+  emailValidationState() {
+    let emailStyle;
+    let length = this.state.email.length;
+    let tail = this.state.email.slice(length - 4, length);
+    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(tail === '.edu' && regex.test(this.state.email)) {
+      emailStyle = 'success';
+    } else if(length > 0) {
+      emailStyle = 'error';
+    }
+    let emailValid = emailStyle === 'success';
+    
+    return { emailStyle, emailValid };
+  }
+
+  passwordValidationState() {
+    let pwStyle;
+    let regex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
+    if(regex.test(this.state.password)) {
+      pwStyle = 'success';
+    } else if(this.state.password.length > 0) {
+      pwStyle = 'error';
+    }
+    let pwValid = pwStyle === 'success';
+
+    return { pwStyle, pwValid }
+  }
+
+  passwordConfirmValidationState() {
+    let pw2Style;
+    if(this.state.passwordConfirm === this.state.password && this.state.passwordConfirm.length > 0) {
+      pw2Style = 'success';
+    } else if(this.state.passwordConfirm.length > 0) {
+      pw2Style = 'error';
+    }
+    let pw2Valid = pw2Style === 'success';
+
+    return { pw2Style, pw2Valid };
   }
 
   handleChange() {
@@ -68,37 +133,24 @@ export class SignupModal extends React.Component {
       username: this.refs.username.getValue(),
       password: this.refs.password.getValue(),
       passwordConfirm: this.refs.passwordConfirm.getValue()
-    })
+    });
+    this.setState(usernameValidationState());
+    this.setState(emailValidationState());
+    this.setState(passwordValidationState());
+    this.setState(passwordConfirmValidationState());
   }
 
-  usernameValidationState() {
-    let length = this.state.username.length;
-    if(length >= 4) return 'success' 
-    else if(length > 0) return 'error';
-  }
-
-  emailValidationState() {
-    let length = this.state.email.length;
-    let tail = this.state.email.slice(length - 4, length);
-    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(tail === '.edu' && regex.test(this.state.email)) return 'success';
-    else if(length > 0) return 'error';
-  }
-
-  passwordValidationState() {
-    let regex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
-    if(regex.test(this.state.password)) return 'success';
-    else if(this.state.password.length > 0) return 'error';
-  }
-
-  passwordConfirmValidationState() {
-    if(this.state.passwordConfirm === this.state.password && this.state.passwordConfirm.length > 0) return 'success';
-    else if(this.state.passwordConfirm.length > 0) return 'error';
+  handleSubmit() {
+    if(this.state.usernameValid && this.state.emailValid && this.state.pwValid && this.state.pw2Valid && this.state.schoolValid) {
+      console.log("all inputs valid")
+    } else {
+      console.log("need to fix form before submit")
+    }
   }
 
   render() {
     var schools = [['wellesley', 'Wellesley College'], ['mit', "MIT"]];
-    var schoolOptions = schools.map((school, i) => { //TODO: fix dropdown
+    var schoolOptions = schools.map((school, i) => { //TODO: fix dropdown + school & email compare
       return(
         <option key={i} value={school[0]}>{school[1]}</option>
       )
@@ -106,8 +158,7 @@ export class SignupModal extends React.Component {
     return (
       <div>
         <Modal.Body>
-          <Input type="select" label="Select a school" value={this.state.school}>
-            <option selected>Select your institution</option>
+          <Input type="select" defaulValue="Select a school">
             {schoolOptions}
           </Input>
           <Input 
@@ -116,30 +167,30 @@ export class SignupModal extends React.Component {
             placeholder="School email" 
             ref="email"
             onChange={this.handleChange}
-            bsStyle={this.emailValidationState()} hasFeedback
+            bsStyle={this.state.emailStyle} hasFeedback
             help="Enter your valid school email address with .edu ending" />
           <Input 
             type="text" 
             value={this.state.username} 
             placeholder="Username" 
             ref="username"
-            bsStyle={this.usernameValidationState()} hasFeedback
+            bsStyle={this.state.usernameStyle} hasFeedback
             onChange={this.handleChange} />
           <Input 
             type="text" 
             value={this.state.password} 
             placeholder="Password"
             ref="password"
-            bsStyle={this.passwordValidationState()} hasFeedback
+            bsStyle={this.state.pwStyle} hasFeedback
             onChange={this.handleChange} />
           <Input 
             type="text" 
             value={this.state.passwordConfirm} 
             placeholder="Re-enter password"
             ref="passwordConfirm"
-            bsStyle={this.passwordConfirmValidationState()} hasFeedback
+            bsStyle={this.state.pw2Style} hasFeedback
             onChange={this.handleChange} />
-          <ButtonInput value="Sign Up" />
+          <ButtonInput value="Sign Up" onClick={this.handleSubmit} />
         </Modal.Body>
       </div>
 
