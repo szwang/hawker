8b3576851a8af6b9713657aaef918d221fd6575e
Suzanne Wang <suzanneswang@gmail.com>
2016-01-09 19:48:05 -0500

    [feat] add email, username, password validation

--- src/js/components/AuthModals.react.js
+++ src/js/components/AuthModals.react.js
@@ -47,8 +47,6 @@ export class LoginModal extends React.Component {
 
 export class SignupModal extends React.Component {
 
-  //TODO add validation
-
   constructor(props) {
     super(props);
 
@@ -61,6 +59,7 @@ export class SignupModal extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.passwordValidationState = this.passwordValidationState.bind(this);
   }
 
   handleChange() {
@@ -68,42 +67,76 @@ export class SignupModal extends React.Component {
       email: this.refs.email.getValue(),
       username: this.refs.username.getValue(),
       password: this.refs.password.getValue(),
-      passwordConfirm: this.refs.passwordConfirm.getValue(),
-      school: this.refs.school.getValue()
+      passwordConfirm: this.refs.passwordConfirm.getValue()
     })
   }
 
+  usernameValidationState() {
+    let length = this.state.username.length;
+    if(length >= 4) return 'success' 
+    else if(length > 0) return 'error';
+  }
+
+  emailValidationState() {
+    let length = this.state.email.length;
+    let tail = this.state.email.slice(length - 4, length);
+    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(tail === '.edu' && regex.test(this.state.email)) return 'success';
+    else if(length > 0) return 'error';
+  }
+
+  passwordValidationState() {
+    let regex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
+    if(regex.test(this.state.password)) return 'success';
+    else if(this.state.password.length > 0) return 'error';
+  }
+
+  passwordConfirmValidationState() {
+    if(this.state.passwordConfirm === this.state.password && this.state.passwordConfirm.length > 0) return 'success';
+    else if(this.state.passwordConfirm.length > 0) return 'error';
+  }
+
   render() {
+    var schools = [['wellesley', 'Wellesley College'], ['mit', "MIT"]];
+    var schoolOptions = schools.map((school, i) => { //TODO: fix dropdown
+      return(
+        <option key={i} value={school[0]}>{school[1]}</option>
+      )
+    })
     return (
       <div>
         <Modal.Body>
-          <Input type="select" value={this.state.school} ref="school">
-            <option value="default">Select school</option>
-            <option value="...">...</option>
+          <Input type="select" value={this.state.school}>
+            {schoolOptions}
           </Input>
           <Input 
             type="text" 
             value={this.state.email} 
             placeholder="School email" 
             ref="email"
-            onChange={this.handleChange} />
+            onChange={this.handleChange}
+            bsStyle={this.emailValidationState()}
+            help="Enter your valid school email address with .edu ending" />
           <Input 
             type="text" 
             value={this.state.username} 
             placeholder="Username" 
             ref="username"
+            bsStyle={this.usernameValidationState()}
             onChange={this.handleChange} />
           <Input 
             type="text" 
             value={this.state.password} 
             placeholder="Password"
             ref="password"
+            bsStyle={this.passwordValidationState()}
             onChange={this.handleChange} />
           <Input 
             type="text" 
             value={this.state.passwordConfirm} 
             placeholder="Re-enter password"
-            ref="passwordConfim"
+            ref="passwordConfirm"
+            bsStyle={this.passwordConfirmValidationState()}
             onChange={this.handleChange} />
           <ButtonInput value="Sign Up" />
         </Modal.Body>
